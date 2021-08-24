19a8374ed21de566c0bec4b8ddd454338e597574
Jack <abdulrehman.6ab@gmail.com>
2021-08-24 17:00:55 +0500

    added redis to store online users

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const {Server} = require('socket.io')
+var redis = require('redis');
+const {promisify} = require('util');
 
 
 const authRoutes = require('./routes/authRoutes')
@@ -17,6 +19,24 @@ const getChatId = require('./helper/getChatID')
 
 const MONGOURI = 'mongodb://127.0.0.1:27017/focial?gssapiServiceName=mongodb';
 const app = express();
+const client = redis.createClient();
+
+client.on("error", function (error) {
+    console.error(error);
+});
+
+client.on('connect', function () {
+    console.log('Connected!');
+});
+
+client.flushall();
+
+const getAsync = promisify(client.get).bind(client);
+const lrangeAsync = promisify(client.lrange).bind(client);
+const lpushAsync = promisify(client.lpush).bind(client);
+const lremAsync = promisify(client.lrem).bind(client);
+const llenAsync = promisify(client.llen).bind(client);
+// client.set("name", "abdul")
 
 
 const storage = multer.diskStorage({
@@ -85,11 +105,10 @@ mongoose.connect(MONGOURI,
     })
 
 
-
 console.log('connected! at post 3344')
 let server = app.listen(3344)
 
-const io = new Server(server,{
+const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000",
         methods: ["GET", "POST"]
@@ -100,32 +119,27 @@ const io = new Server(server,{
 let onlineUsers = {};
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    // console.log('a user connected');
 
-    socket.on('disconnect', () => {
+    socket.on('disconnect', async () => {
+        // console.log(socket.userID)
+        if (!socket.userID) return;
 
-        console.log(onlineUsers);
-        if (!onlineUsers[socket.userID]) return
+        let length = await llenAsync(socket.userID);
+        if (Number(length) === 0) return
 
-        let currentIdIndex = onlineUsers[socket.userID].findIndex(id=>id === socket.id);
+        client.lrem(socket.userID, 1, socket.id);
+        // console.log('user closed a connection');
 
-        if (currentIdIndex>=0){
-            onlineUsers[socket.userID].splice(currentIdIndex,1);
-        }
-
-        console.log('user closed a connection');
-
-        if (onlineUsers[socket.userID].length === 0) {
+        length = await llenAsync(socket.userID);
+        if (Number(length) === 0) {
             socket.emit("offline", socket.userID);
-            delete onlineUsers[socket.userID]
-            console.log('user disconnected');
+            // console.log('user disconnected');
         }
-
-        console.log(onlineUsers)
 
     });
 
-    socket.on('join', (data) => {
+    socket.on('join', async (data) => {
 
         const isAuth = verifyToken(data.token);
 
@@ -135,25 +149,25 @@ io.on('connection', (socket) => {
             return
         }
 
-        if (!onlineUsers[isAuth.userID])  onlineUsers[isAuth.userID] = []
-
         socket.userID = isAuth.userID;
-        onlineUsers[isAuth.userID].push(socket.id);
+        // console.log(socket.userID)
 
+        client.lpush(isAuth.userID, socket.id);
         socket.join(isAuth.userID);
+
     });
 
-    socket.on('chat message',async (msg)=>{
-        console.log('message=',msg)
-        let chatID = getChatId(msg.to,msg.from);
-        console.log('message=',chatID);
+    socket.on('chat message', async (msg) => {
+        console.log('message=', msg)
+        let chatID = getChatId(msg.to, msg.from);
+        console.log('message=', chatID);
 
         let newMsg = new Message({
-            chatID:chatID,
-            to:msg.to,
-            from:msg.from,
-            value:msg.value,
-            isRead:false,
+            chatID: chatID,
+            to: msg.to,
+            from: msg.from,
+            value: msg.value,
+            isRead: false,
         })
 
         await newMsg.save()
@@ -161,11 +175,11 @@ io.on('connection', (socket) => {
         socket.in(msg.to).emit('new chat message', msg);
     })
 
-    socket.on('typingStart',async (data)=>{
+    socket.on('typingStart', async (data) => {
         socket.in(data.to).emit('notifyTypingStart');
     })
 
-    socket.on('typingStop',async (data)=>{
+    socket.on('typingStop', async (data) => {
         socket.in(data.to).emit('notifyTypingStop');
     })
 
