7f56173784dbae5988e2121d37cf284e298700a8
Jack <abdulrehman.6ab@gmail.com>
2021-09-13 08:37:14 +0500

    update react api url

--- client/src/assets/config/configs.js
+++ client/src/assets/config/configs.js
@@ -1,5 +1,5 @@
 const configs = {
-    api_url: process.env.API_URL || "http://localhost:3344"
+    api_url: process.env.REACT_APP_VERCEL_ENV? "https://social-media-site-bay.vercel.app/" : "http://localhost:3344"
 }
 
 export default configs
